8a154256289850b5994f43550492481118090c1f
Pablo-Lemos <plemos91@gmail.com>
2023-06-06 10:53:44 -0400

    Added setup.py to make pip installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sapphire',
+      version='0.1',
+      description='Sapphire',
+      url='https://github.com/virajpandya/sapphire',
+      author='Viraj Pandya',
+      author_email='vgp2108@columbia.edu',
+      license='MIT',
+      packages=['sapphire'],
+      zip_safe=False)
